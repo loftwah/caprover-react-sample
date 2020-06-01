92cc31b4d3460997430867d6f83bb2d4656d1b35
Benoît Hubert <benoithubert@gmail.com>
2020-06-01 10:13:38 +0200

    Prepare React app for CapRover deployment

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [obj, setObj] = useState(null);
+  useEffect(
+    () => {
+      fetch(`${process.env.REACT_APP_API_BASE_URL}/api/meme`)
+        .then((res) => res.json())
+        .then((data) => setObj(data))
+        .catch((err) => alert(err.message));
+    },
+    []
+  );
+  return (
+    <div>
+      <h1>Sample React App</h1>
+      <div>
+Server responded with this data:
+        {
+	  obj && (
+	    <div>
+		<img src={obj.pictureUrl} alt="Meme" />
+		<p>{obj.title}</p>
+            </div>
+	  )
+	}
+      </div>
+    </div>
+  );
+}
+
+export default App;
